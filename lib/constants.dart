abdb2cabc63b45d6a4d5f1b2ea67ad699107ebeb
aymanSE <61785906+aymanSE@users.noreply.github.com>
2023-03-23 00:55:50 +0300

    ayman's task

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const largeScreen = 1336; //any thing greater is website
+  static const smallScreen = 360; //any thing smaller is mobile
+  static const white = Color(0xFFFFFFFF);
+  static const sand = Color(0xFFE1BC6D);
+  static const wine = Color(0xFF591C1B);
+  static const black = Color(0xFF1E1C1C);
+
+  //and in between is tablet and others if we want to consider them
+}
