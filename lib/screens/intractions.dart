2f026e46af0e828a194c5adf42fa91a033de35a2
aymanSE <61785906+aymanSE@users.noreply.github.com>
2023-06-24 17:39:29 +0300

    display intraction for memory

--- lib/screens/intractions.dart
+++ lib/screens/intractions.dart
@@ -1,3 +1,4 @@
+import 'package:clust/providers/event_spot_provider.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:clust/styles/mobile_styles.dart' as mobile;
@@ -9,26 +10,26 @@ import '../controllers/answer_controller.dart';
 
 class QuestionScreen extends StatelessWidget {
   final int interactionId;
-
-  QuestionScreen({required this.interactionId});
+  final bool ispast;
+  QuestionScreen({required this.interactionId,required this.ispast});
 
   @override
   Widget build(BuildContext context) {
-    
-  ThemeData theme(BuildContext context) {
-    return ThemeData(
-      textTheme: Theme.of(context).textTheme.copyWith(
-            displayLarge: mobile.displayLarge(color: Colors.amber),
-            headlineLarge: mobile.headlineLarge(color: Palate.black),
-            headlineMedium: mobile.headlineMedium(color: Palate.black),
-            headlineSmall: mobile.headlineSmall(color: Palate.black),
-            bodySmall: mobile.bodySmall(color: Palate.black),
-            labelSmall: mobile.labelSmall(color: Palate.black),
-          ),
-    );
-  }
+    ThemeData theme(BuildContext context) {
+      return ThemeData(
+        textTheme: Theme.of(context).textTheme.copyWith(
+              displayLarge: mobile.displayLarge(color: Colors.amber),
+              headlineLarge: mobile.headlineLarge(color: Palate.black),
+              headlineMedium: mobile.headlineMedium(color: Palate.black),
+              headlineSmall: mobile.headlineSmall(color: Palate.black),
+              bodySmall: mobile.bodySmall(color: Palate.black),
+              labelSmall: mobile.labelSmall(color: Palate.black),
+            ),
+      );
+    }
+
     return Theme(
-      data: !kIsWeb ? theme(context) : ThemeData(), 
+      data: !kIsWeb ? theme(context) : ThemeData(),
       child: Scaffold(
         appBar: AppBar(
           title: Text('Interactions'),
@@ -37,13 +38,14 @@ class QuestionScreen extends StatelessWidget {
         body: FutureBuilder<Interaction>(
           future: InteractionController().getByID(interactionId),
           builder: (context, snapshot) {
-            
             if (snapshot.connectionState == ConnectionState.waiting) {
               return Center(child: CircularProgressIndicator());
             } else if (snapshot.hasError) {
               return Center(child: Text('Error: ${snapshot.error}'));
             } else if (snapshot.hasData) {
               Interaction interaction = snapshot.data!;
+              eventSpotProvider provider = eventSpotProvider();
+             
               return Padding(
                 padding: EdgeInsets.all(20),
                 child: Column(
@@ -81,7 +83,7 @@ class QuestionScreen extends StatelessWidget {
             }
           },
         ),
-        floatingActionButton: FloatingActionButton(
+        floatingActionButton:ispast? null:FloatingActionButton(
           onPressed: () {
             _showAnswerDialog(context);
           },
@@ -93,91 +95,89 @@ class QuestionScreen extends StatelessWidget {
         ),
       ),
     );
-    
   }
 
- void _showAnswerDialog(BuildContext context) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      TextEditingController answerController = TextEditingController();
+  void _showAnswerDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        TextEditingController answerController = TextEditingController();
 
-      return Dialog(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12.0),
-        ),
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
+        return Dialog(
+          shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12.0),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                'Enter your answer',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(height: 16.0),
-              TextField(
-                controller: answerController,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    child: Text(
-                      'Cancel',
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
+          child: Container(
+            padding: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Enter your answer',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
-                  SizedBox(width: 8.0),
-                  TextButton(
-                    onPressed: () async {
-                      String answerText = answerController.text;
-                      if (answerText.isNotEmpty) {
-                        Answer answer = Answer(
-                          0, // Provide the actual answer ID
-                          answerText,
-                          interactionId,
-                        );
-                        await AnswerController().create(answer);
-
+                ),
+                SizedBox(height: 16.0),
+                TextField(
+                  controller: answerController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
                         Navigator.pop(context);
-                      }
-                    },
-                    child: Text(
-                      'Save',
-                      style: TextStyle(
-                        color: Colors.green,
+                      },
+                      child: Text(
+                        'Cancel',
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      );
-    },
-  );
-}
+                    SizedBox(width: 8.0),
+                    TextButton(
+                      onPressed: () async {
+                        String answerText = answerController.text;
+                        if (answerText.isNotEmpty) {
+                          Answer answer = Answer(
+                            0, // Provide the actual answer ID
+                            answerText,
+                            interactionId,
+                          );
+                          await AnswerController().create(answer);
 
+                          Navigator.pop(context);
+                        }
+                      },
+                      child: Text(
+                        'Save',
+                        style: TextStyle(
+                          color: Colors.green,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
 }
 
 class AnswerList extends StatefulWidget {
